42bc594ccf4a8aa888eda0b906fb7ab3be1ce036
Dshisher <dshisher@gmail.com>
2023-02-06 00:14:03 +0200

    had some progress with the project - added some new functions that will be used in the future.

---
+++ functions_bus_stops.py
@@ -0,0 +1,50 @@
+"""
+This file contains function for the data extracted from: https://data.gov.il/dataset/bus_stops/resource/e873e6a2-66c1-494f-a677-f5e77348edb0.
+
+"""
+
+# imports:
+import os
+import requests
+#
+import pandas as pd
+
+
+def download_data(num_limit=500):
+    """
+    Download data from: https://data.gov.il/dataset/bus_stops/resource/e873e6a2-66c1-494f-a677-f5e77348edb0
+
+    :param num_limit: Number of data values to fetch from the website.
+    :return df: Dataframe containing the scrapped data.
+    """
+
+    check_if_data_cache_exists()
+
+    if num_limit == 0:
+        path_to_data = 'https://data.gov.il/api/3/action/datastore_search?resource_id=e873e6a2-66c1-494f-a677-f5e77348edb0'
+    else:
+        path_to_data = f'https://data.gov.il/api/3/action/datastore_search?resource_id=e873e6a2-66c1-494f-a677-f5e77348edb0&limit={num_limit}'  # &limit=5
+
+    data = requests.get(path_to_data)
+
+    output_json = data.json()
+    lines = output_json['result']['records']
+    df = pd.DataFrame.from_records(lines)
+
+    return df
+
+
+def check_if_data_cache_exists(num_limit=0, force_re_downloading=False):
+    """
+
+    :param num_limit: Number of data values to fetch from the website.
+    :param force_re_downloading:
+    :return Dataframe containing the scrapped data:
+    """
+    if not force_re_downloading and "bus_stations.csv" in os.listdir():
+        df_bus_stations = pd.read_csv("bus_stations.csv")
+    else:
+        df_bus_stations = download_data()
+        df_bus_stations.to_csv("bus_stations.csv")
+
+    return df_bus_stations
--- get_bus_stops.py
+++ get_bus_stops.py
@@ -5,26 +5,39 @@ This script downloads the bus stops data.
 
 import requests
 #
-import pandas as pd
+import functions_bus_stops as fs
 
 
-def download_data():
-    """
-    Download data from: https://data.gov.il/dataset/bus_stops/resource/e873e6a2-66c1-494f-a677-f5e77348edb0
-
-    :return df: Dataframe containing the scrapped data.
+def get_destination_coordinates(api_key, destination):
     """
 
-    path_to_data = 'https://data.gov.il/api/3/action/datastore_search?resource_id=e873e6a2-66c1-494f-a677-f5e77348edb0&limit=500'  # &limit=5
-    data = requests.get(path_to_data)
+    :param api_key:
+    :param destination:
+    :return destination_data: dictionary with destination data.
+    """
+    places_url = f"https://maps.googleapis.com/maps/api/geocode/json?address={destination}&key={api_key}"
+    destination_response = requests.get(places_url)
+    destination_data     = destination_response.json()
+    return destination_data
 
-    output_json = data.json()
-    lines = output_json['result']['records']
-    df = pd.DataFrame.from_records(lines)
 
-    return df
+def main(destination, walking_radius=500):
+    """
+    :param destination:
+    :param walking_radius: max distance between closest but station to destination.
+    :return:
+    """
+
+    # Read API key:
+    with open('key_google_api.txt') as f:
+        api_key = f.readlines()[0]
+
+    get_destination_coordinates(api_key, destination)
+
+    df_bus_stations = fs.check_if_data_cache_exists()
+    # df_bus_stations['distance'] =
 
 
 if __name__ == '__main__':
-    download_data()
-
+    destination_test = "qiryat+shemona"
+    main(destination_test)
